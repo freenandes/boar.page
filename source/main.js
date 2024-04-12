66106b7e16579c3b21df88fb11838dd23c142a07
pmcf-percona <pedro.fernandes@percona.com>
2024-04-12 23:01:24 +0100

    Layout foundation

--- source/main.js
+++ source/main.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
             exclude: [...document.querySelectorAll('*:not(main)')]
         });
     }
-
     // Put listeners on stickies
     document.querySelectorAll('article p[contenteditable="true"]').forEach(listenPar);
 
@@ -34,8 +33,6 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add `.selection` class to clicked article
         theArticle.classList.add('selection');
     });
-
-
     // Handle keyboard events
     document.addEventListener('keydown', function (event) {
 
@@ -69,13 +66,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             }
         } else {
-
             // Use navigation
             navKeys(event);
         }
     });
 });
-
 // Attaches event listeners to stickies
 function listenPar(editStickie) {
 
@@ -92,42 +87,36 @@ function listenPar(editStickie) {
         charCounter.style.display = 'none'; // Initially hide it
         theArticle.appendChild(charCounter);
     }
-
     // When stickie is focused
     editStickie.addEventListener('focus', function () {
 
         // Show the counter and update it
         charCounter.style.display = '';
         updateCounter(editStickie, charCounter);
-        
+
         // Highlight the focused stickie
         document.querySelectorAll('article').forEach(el => el.classList.remove('selection'));
         theArticle.classList.add('selection');
     });
-
     // Blur event: Hide the counter and remove stickie highlight
-    editStickie.addEventListener('blur', function() {
+    editStickie.addEventListener('blur', function () {
         charCounter.style.display = 'none';
         theArticle.classList.remove('selection');
     });
-
     // Handle paste event: accept, clean up and paste
-    editStickie.addEventListener('paste', function(event) {
+    editStickie.addEventListener('paste', function (event) {
         event.preventDefault();
         const text = (event.clipboardData || window.clipboardData).getData('text/plain');
         document.execCommand('insertText', false, text);
         updateCounter(editStickie, charCounter);
     });
-
     // Input event: Update counter as user types
-    editStickie.addEventListener('input', function() {
+    editStickie.addEventListener('input', function () {
         updateCounter(editStickie, charCounter);
     });
-
     // Add event listener for handling direction changes with keyboard shortcuts
     editStickie.addEventListener('keydown', navKeys);
 }
-
 // Count chars
 function updateCounter(editStickie, charCounter) {
     const textLength = editStickie.innerText.trim().length;
@@ -138,13 +127,11 @@ function updateCounter(editStickie, charCounter) {
         editStickie.classList.remove('has-value');
     }
 }
-
 // Toggle the visual indication of whether a stickie is empty
 function togglePlaceholder(editStickie) {
     const isEmpty = editStickie.innerText.trim() === '';
     editStickie.classList.toggle('has-value', !isEmpty);
 }
-
 // Add a stickie
 function addStickie(refStickie, direction) {
 
@@ -158,8 +145,8 @@ function addStickie(refStickie, direction) {
     // Create a counter for the new stickie
     const charCounter = document.createElement('small');
     charCounter.classList.add('counter');
-    charCounter.style.display = 'none'; // Initially hide the counter
-    charCounter.textContent = '0/240'; // Initialize with 0/240
+    charCounter.style.display = 'none';
+    charCounter.textContent = '0/240';
 
     // Append the paragraph and the counter to the article
     theArticle.appendChild(thePar);
@@ -172,7 +159,6 @@ function addStickie(refStickie, direction) {
     } else {
         refStickie.closest('section').before(theSection);
     }
-
     // Add listeners
     listenPar(thePar);
 
@@ -181,15 +167,16 @@ function addStickie(refStickie, direction) {
     theArticle.classList.add('selection');
     thePar.focus();
 }
-
 // Delete a stickie
 function deleteStickie(theArticle) {
+
     // Find the closest parent
     const theSection = theArticle.closest('section');
 
     // Determine the next element to focus on after the deletion
     let nextElement = theSection.nextElementSibling || theSection.previousElementSibling;
     if (nextElement) {
+
         // Find the stickie and focus on it
         let nextP = nextElement.querySelector('p[contenteditable="true"]');
         if (nextP) {
@@ -199,7 +186,6 @@ function deleteStickie(theArticle) {
     // Actually delete the stickie
     theSection.remove();
 }
-
 // Handle navigation keys to change the selection of stickies
 function navKeys(event) {
 
@@ -210,7 +196,6 @@ function navKeys(event) {
         'ArrowLeft': 'west',
         'ArrowUp': 'north'
     };
-
     // Check if both Ctrl (Cmd on Mac) and Alt are pressed along with the direction keys
     if (event.ctrlKey && event.altKey && Object.keys(directionMap).includes(event.key)) {
         event.preventDefault();
